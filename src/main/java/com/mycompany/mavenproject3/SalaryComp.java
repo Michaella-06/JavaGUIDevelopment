e0374ed99e38759fe569239ec186d2f21f927e41
Michaella Eroles <Michaella Eroles@LAPTOP-CEA6UT30>
2025-06-24 23:00:57 +0800

    First Commit

---
+++ src/main/java/com/mycompany/mavenproject3/SalaryComp.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject3;
+
+/**
+ *
+ * @author Michaella Eroles
+ */
+public class SalaryComp {
+    public double netSalary(double h, double r, double t) {
+        double gross = h * r;
+        double taxAmount = gross * t;
+        double netSalary = gross - taxAmount;
+        return netSalary;
+    }
+    public int netSalary(int h, int r, int t) {
+        int gross = h * r;
+        int taxAmount = (gross * t)/100;
+        int netSalary = gross - taxAmount;
+        return netSalary;
+    }
+}
