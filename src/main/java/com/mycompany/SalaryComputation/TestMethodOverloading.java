a4719c6190004ceae8958301634c4eb464c9924f
Michaella Eroles <Michaella Eroles@LAPTOP-CEA6UT30>
2025-06-25 10:50:39 +0800

    First Commit

--- src/main/java/com/mycompany/SalaryComputation/TestMethodOverloading.java
+++ src/main/java/com/mycompany/SalaryComputation/TestMethodOverloading.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package com.mycompany.mavenproject3;
+package com.mycompany.SalaryComputation;
 
+import com.mycompany.SalaryComputation.SalaryComp;
 import javax.swing.JOptionPane;
 
 /**
